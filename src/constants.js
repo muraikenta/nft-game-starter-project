523268df8d5657bc2705fa2bbc4e5e266447fb7d
muraikenta <muraikenta0507@gmail.com>
2022-08-17 00:37:28 +0900

    upload to github

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+// constants.js
+const CONTRACT_ADDRESS = "0x38A23D085F957891BA64AB77AE07293FeA8Df68F";
+
+//NFT キャラクター の属性をフォーマットしてオブジェクトとして返します。
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+// 変数を constants.js 以外の場所でも使えるようにします。
+export { CONTRACT_ADDRESS, transformCharacterData };
